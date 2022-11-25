31834cb5d3db6e7b3376e58fd5bbf8eaae7cbcfe
ana-12f <116896656+ana-12f@users.noreply.github.com>
2022-11-24 20:08:49 -0600

    Add files via upload

---
+++ Calculador de prestamos/index.js
@@ -0,0 +1,12 @@
+function calculateLoan() {
+    const amount = document.getElementById("amount").value
+    const interest_rate = document.getElementById("interest_rate").value
+    const months = document.getElementById("months").value
+    const interest = (amount * (interest_rate * .01)) / months
+
+    const payment = (amount / months + interest).toFixed(2)
+
+    document.getElementById("payment").innerHTML = `pago total: ${payment}`
+
+
+}
